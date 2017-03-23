8764708d6bba1c2682a118436df10c6a7d0ecb9e
Marshajv <Marshajv@gmatc.matc.edu>
2017-03-22 20:05:43 -0500

    Initial Commit
    has the base of the golf game
    fix inheritience shit and what not

---
+++ GolfGame/Assets/Scripts/ClubInherit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClubInherit : GolfSwing {
+    public int maxSwingPower;
+    public int curSwingPower;
+
+    float timer;
+    public float incTime;
+
+    public bool increasingForce;
+
+    public Rigidbody golfy;
+    public enum State
+    {
+        aiming,
+        swinging,
+        striking
+    }
+    GolfSwing golfSwing;
+    void Start()
+    {
+         golfSwing = new GolfSwing();
+    }
+
+    public void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            timer = Time.time;
+            if (timer % 1 == 0)
+            {
+                if (curSwingPower < maxSwingPower)
+                {
+                    curSwingPower++;
+                }
+            } 
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            golfSwing.IncreaseForce(golfy,curSwingPower);
+            timer = 0;
+        }
+    }
+}
---
+++ GolfGame/Assets/Scripts/GolfSwing.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GolfSwing : MonoBehaviour {
+    public void IncreaseForce(Rigidbody rb, float force)
+    {
+        print("Increasing force");
+            rb.AddForce(transform.forward*force);
+    }
+}
